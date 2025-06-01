7a7d9f081990a521f9e007659a5e8da3d5375382
lwinhtoosan <lwinhtoosan2001@gmail.com>
2025-06-01 10:48:32 +0630

    imp: home screen

---
+++ src/components/Tutorail1.js
@@ -0,0 +1,11 @@
+"use client"
+import React from "react";
+export default class Person extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
